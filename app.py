77361c5f30205f2274924746b82e3b0e772d899c
Donald Vallejo <donny.vallejo@gmail.com>
2019-09-30 19:42:53 -0700

    fixing rout to heroku and idk

--- app.py
+++ app.py
@@ -1,21 +1,25 @@
 # https://playlisterdv.herokuapp.com/ | https://git.heroku.com/playlisterdv.git
 # https://playlister2.herokuapp.com/ | https://git.heroku.com/playlister2.git
 
+# sudo lsof -iTCP -sTCP:LISTEN -n -P
+
 import os
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
 
 """When im using heroku"""
-# host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
+print(host)
+client = MongoClient(host=host)
 # client = MongoClient(host=f'{host}?retryWrites=false')
-# db = client.get_default_database()
-# playlists = db.playlists
+db = client.get_default_database()
+playlists = db.playlists
 
 """When im not using heroku"""
-client = MongoClient()
-db = client.Playlister
-playlists = db.playlists
+# client = MongoClient()
+# db = client.Playlister
+# playlists = db.playlists
 
 
 app = Flask(__name__)
@@ -24,8 +28,7 @@ app = Flask(__name__)
 @app.route('/')
 def playlists_index():
     """Show all playlists."""
-    print("playlist_index",playlists_index)
-    return render_template('templates/playlists_index.html', playlists=playlists.find()) 
+    return render_template('playlists_index.html', playlists=playlists.find()) 
 
 
 @app.route('/playlists', methods=['POST'])
@@ -46,32 +49,32 @@ def playlists_new():
     return render_template('playlists_new.html', playlist={}, title='New Playlist')    
 
 
-# @app.route('/playlists/<playlist_id>/edit')
-# def playlists_edit(playlist_id):
-#     """Show the edit form for a playlist."""
-#     playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-#     return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
 
 
-# @app.route('/playlists/<playlist_id>', methods=['POST'])
-# def playlists_update(playlist_id):
-#     """Submit an edited playlist."""
-#     updated_playlist = {
-#         'title': request.form.get('title'),
-#         'description': request.form.get('description'),
-#         'videos': request.form.get('videos').split()
-#     }
-#     playlists.update_one(
-#         {'_id': ObjectId(playlist_id)},
-#         {'$set': updated_playlist})
-#     return redirect(url_for('playlists_show', playlist_id=playlist_id))
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+    """Submit an edited playlist."""
+    updated_playlist = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'videos': request.form.get('videos').split()
+    }
+    playlists.update_one(
+        {'_id': ObjectId(playlist_id)},
+        {'$set': updated_playlist})
+    return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 
-# @app.route('/playlists/<playlist_id>/delete', methods=['POST'])
-# def playlists_delete(playlist_id):
-#     """Delete one playlist."""
-#     playlists.delete_one({'_id': ObjectId(playlist_id)})
-#     return redirect(url_for('playlists_index'))
+@app.route('/playlists/<playlist_id>/delete', methods=['POST'])
+def playlists_delete(playlist_id):
+    """Delete one playlist."""
+    playlists.delete_one({'_id': ObjectId(playlist_id)})
+    return redirect(url_for('playlists_index'))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
