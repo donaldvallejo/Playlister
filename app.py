aded23e0c638daac662b1d09005b0c5435b23311
Donald Vallejo <donny.vallejo@gmail.com>
2019-10-02 18:28:40 -0700

    oh shit i hope this works and fuck you heroku

--- app.py
+++ app.py
@@ -2,94 +2,44 @@
 # https://playlister2.herokuapp.com/ | https://git.heroku.com/playlister2.git
 
 # sudo lsof -iTCP -sTCP:LISTEN -n -P
-'''imports'''
-
 import os
 from datetime import datetime
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
-
-"""When im using heroku"""
-
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Playlister')
 client = MongoClient(host=host)
+# client = MongoClient(host=f'{host}?retryWrites=false')
 # client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
 playlists = db.playlists
 comments = db.comments
 
-
-"""When im not using heroku"""
-
-client = MongoClient()
-db = client.Playlister
-playlists = db.playlists
-
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def playlists_index():
-    """Show all playlists."""
-    return render_template('playlists_index.html', playlists=playlists.find()) 
-
-
-@app.route('/playlists', methods=['POST'])
-def playlists_submit():
-    """Submit a new playlist."""
-    playlist = {
-        'title': request.form.get('title'),
-        'description': request.form.get('description'),
-        'videos': request.form.get('videos').split(),
-        'created_at': datetime.now()
-    }
-    print(playlist)
-    playlist_id = playlists.insert_one(playlist).inserted_id
-    return redirect(url_for('playlists_show', playlist_id=playlist_id))
-
-'''
-@app.route('/playlists', methods=['POST'])
-def playlists_submit():
-    """Submit a new playlist."""
-    playlist = {
-        'title': request.form.get('title'),
-        'description': request.form.get('description'),
-        'videos': request.form.get('videos').split()
-    }
-    playlist_id = playlists.insert_one(playlist).inserted_id
-    #return redirect(url_for('playlists_show', playlist=playlist, playlist_id=playlist_id))
-    return render_template('playlists_show.html', playlist=playlist, playlist_id=playlist_id)
-#'''
+  """Show all playlist"""
+  return render_template('playlists_index.html', playlists=playlists.find())
 
 @app.route('/playlists/new')
 def playlists_new():
-    """Create a new playlist."""
-    return render_template('playlists_new.html', playlist={}, title='New Playlist')    
+  """Create Playlists"""
+  return render_template('playlists_new.html', playlist = {}, title='New Playlist')
 
+@app.route('/playlists', methods=['POST'])
+def playlists_submit():
+  """Submit playlists to database"""
+  # base_url = 'https://youtube.com/embed/'
+  playlist = {
+    'title': request.form.get('title'),
+    'description': request.form.get('description'),
+    'videos':  request.form.get('videos').split(),
+    'created_at': datetime.now()
+  }
 
-@app.route('/playlists/<playlist_id>/edit')
-def playlists_edit(playlist_id):
-    """Show the edit form for a playlist."""
-    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
-    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
-
-
-@app.route('/playlists/<playlist_id>', methods=['POST'])
-def playlists_update(playlist_id):
-    """Submit an edited playlist."""
-    # print(request)
-    # print([key for key in request.form.keys()])
-    updated_playlist = {
-        'title': request.form.get('title'),
-        'description': request.form.get('description'),
-        'videos': request.form.get('videos').split()
-    }
-    playlists.update_one(
-        {'_id': ObjectId(playlist_id)},
-        {'$set': updated_playlist})
-    return redirect(url_for('playlists_show', playlist_id=playlist_id))
+  playlist_id = playlists.insert_one(playlist).inserted_id
+  return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 @app.route('/playlists/<playlist_id>')
 def playlists_show(playlist_id):
@@ -98,13 +48,30 @@ def playlists_show(playlist_id):
     playlist_comments = comments.find({'playlist_id': ObjectId(playlist_id)})
     return render_template('playlists_show.html', playlist=playlist, comments=playlist_comments)
 
+@app.route('/playlists/<playlist_id>/edit')
+def playlists_edit(playlist_id):
+    """Show the edit form for a playlist."""
+    playlist = playlists.find_one({'_id': ObjectId(playlist_id)})
+    return render_template('playlists_edit.html', playlist=playlist, title='Edit Playlist')
+
+@app.route('/playlists/<playlist_id>', methods=['POST'])
+def playlists_update(playlist_id):
+  """Submit an edited playlists"""
+  updated_playlist = {
+      'title': request.form.get('title'),
+      'description': request.form.get('description'),
+      'videos': request.form.get('videos').split()
+  }
+  playlists.update_one(
+    {'_id': ObjectId(playlist_id)},
+    {'$set': updated_playlist})
+  return redirect(url_for('playlists_show', playlist_id=playlist_id))
 
 @app.route('/playlists/<playlist_id>/delete', methods=['POST'])
 def playlists_delete(playlist_id):
     """Delete one playlist."""
     playlists.delete_one({'_id': ObjectId(playlist_id)})
     return redirect(url_for('playlists_index'))
-
 
 @app.route('/playlists/comments', methods=['POST'])
 def comments_new():
@@ -114,12 +81,8 @@ def comments_new():
         'content': request.form.get('content'),
         'playlist_id': ObjectId(request.form.get('playlist_id'))
     }
-    print(comment)
     comment_id = comments.insert_one(comment).inserted_id
     return redirect(url_for('playlists_show', playlist_id=request.form.get('playlist_id')))
-    #return render_template('playlists_show.html', playlist_id=request.form.get('playlist_id'))
-    #return render_template('playlists_show.html', playlist=playlist, playlist_id=comment['playlist_id'])
-
 
 @app.route('/playlists/comments/<comment_id>', methods=['POST'])
 def comments_delete(comment_id):
@@ -128,6 +91,5 @@ def comments_delete(comment_id):
     comments.delete_one({'_id': ObjectId(comment_id)})
     return redirect(url_for('playlists_show', playlist_id=comment.get('playlist_id')))
 
-
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
